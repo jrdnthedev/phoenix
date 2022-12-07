37105a6d6578221ed5499a2d828fccb1adc521df
smooooky <75350537+smooooky@users.noreply.github.com>
2022-12-07 18:42:02 -0500

    added dummy save function

--- src/app/features/my-list/components/item-entry/item-entry.component.ts
+++ src/app/features/my-list/components/item-entry/item-entry.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ListItem } from '../../interfaces/list-item';
 
 @Component({
   selector: 'app-item-entry',
@@ -6,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./item-entry.component.scss'],
 })
 export class ItemEntryComponent implements OnInit {
+  @Input() title: string;
+  @Input() name: string;
+  @Input() beer: string;
+  @Input() type: string;
+  @Input() notes: string;
+  @Input() location: any;
+  @Input() price: number;
+  @Input() rating: number;
+  newEntry: any[] = [];
 
   constructor() { }
 
   ngOnInit() {}
 
+  save(): void {
+    this.newEntry.push({
+      name: this.name,
+      beer: this.beer,
+      type: this.type,
+      notes: this.notes,
+      location: this.location,
+      price: this.price,
+      rating: this.rating
+    });
+    console.log('information save!', this.newEntry);
+  }
 }
